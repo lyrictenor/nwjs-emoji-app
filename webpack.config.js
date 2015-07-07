12500e15824f84f2ca54b7072f31f3d5c419bf4a
sanemat <o.gata.ken@gmail.com>
2015-07-07 22:25:58 +0900

    chore(app): remove unnecessary modules

--- webpack.config.js
+++ webpack.config.js
@@ -18,21 +18,12 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
-    new ExtractTextPlugin('app.css', { allChunks: true }),
-    new HtmlWebpackPlugin({
-      title: 'Redux React Router Async Example',
-      filename: 'index.html',
-      template: 'index.template.html',
-      favicon: path.join(__dirname, 'assets', 'images', 'favicon.ico')
-    })
+    new ExtractTextPlugin('app.css', { allChunks: true })
   ],
   module: {
     loaders: [
       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') },
       { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
     ]
-  },
-  cssnext: {
-    browsers: 'last 2 versions'
   }
 };
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -26,21 +25,12 @@ module.exports = {
         warnings: false
       }
     }),
-    new ExtractTextPlugin('app.css', { allChunks: true }),
-    new HtmlWebpackPlugin({
-      title: 'Redux React Router Async Example',
-      filename: 'index.html',
-      template: 'index.template.html',
-      favicon: path.join(__dirname, 'assets/images/favicon.ico')
-    })
+    new ExtractTextPlugin('app.css', { allChunks: true })
   ],
   module: {
     loaders: [
       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') },
       { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ }
     ]
-  },
-  cssnext: {
-    browsers: 'last 2 versions'
   }
 };
