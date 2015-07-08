eb84f62ec9a654f4d01d58b8283bfd91d36d0307
sanemat <o.gata.ken@gmail.com>
2015-07-08 20:31:29 +0900

    chore(lib): comment out unnecessary code

--- lib/Root.jsx
+++ lib/Root.jsx
@@ -1,51 +1,56 @@
 import React, { PropTypes } from 'react'
-import { Redirect, Router, Route } from 'react-router'
+//import { Redirect, Router, Route } from 'react-router'
 import { Provider } from 'redux/react'
-import { createDispatcher, createRedux, composeStores } from 'redux'
-import { loggerMiddleware, thunkMiddleware } from './middleware'
-import * as components from './components'
-import * as stores from './stores'
-
-const {
-  Application,
-  About,
-  GithubStargazers,
-  GithubRepo,
-  GithubUser
-  } = components
-const dispatcher = createDispatcher(
-  composeStores(stores),
-    getState => [ thunkMiddleware(getState), loggerMiddleware ]
-)
-const redux = createRedux(dispatcher)
-
-export default class Root extends React.Component {
-
-  static propTypes = {
-    history: PropTypes.object.isRequired
-  }
+//import { createDispatcher, createRedux, composeStores } from 'redux'
+//import { loggerMiddleware, thunkMiddleware } from './middleware'
+//import * as components from './components'
+//import * as stores from './stores'
+//
+//const {
+//  Application,
+//  About,
+//  GithubStargazers,
+//  GithubRepo,
+//  GithubUser
+//} = components
+//const dispatcher = createDispatcher(
+//  composeStores(stores),
+//  getState => [ thunkMiddleware(getState), loggerMiddleware ]
+//)
+//const redux = createRedux(dispatcher)
+//
+class Root extends React.Component {
+  //
+  //static propTypes = {
+  //  history: PropTypes.object.isRequired
+  //}
 
   render () {
-    const { history } = this.props
+    //const { history } = this.props
     return (
-      <Provider redux={redux}>
-        {renderRoutes.bind(null, history)}
+      <Provider>
+        <h1>Emoji App</h1>
       </Provider>
     )
   }
 }
 
-function renderRoutes (history) {
-  return (
-    <Router history={history}>
-      <Route component={Application}>
-        <Route path="about" component={About} />
-        <Route path="stargazers" component={GithubStargazers}>
-          <Route name='repo' path=':username/:repo' component={GithubRepo} />
-          <Route name='user' path=':username' component={GithubUser} />
-        </Route>
-        <Redirect from="/" to="/stargazers/emmenko" />
-      </Route>
-    </Router>
-  )
+Root.contextTypes = {
+  history: PropTypes.object.isRequired
 }
+
+export default Root
+//function renderRoutes (history) {
+//  return (
+//    <Router history={history}>
+//      <Route component={Application}>
+//        <Route path="about" component={About} />
+//        <Route path="stargazers" component={GithubStargazers}>
+//          <Route name='repo' path=':username/:repo' component={GithubRepo} />
+//          <Route name='user' path=':username' component={GithubUser} />
+//        </Route>
+//        <Redirect from="/" to="/stargazers/emmenko" />
+//      </Route>
+//    </Router>
+//  )
+//}
