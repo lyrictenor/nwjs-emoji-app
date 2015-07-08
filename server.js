a64431b7a6a0a9df0fd36c33f7408dd577eb44d6
sanemat <o.gata.ken@gmail.com>
2015-07-08 16:12:29 +0900

    chore(build): add local/dev

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var config = require('./webpack.config.local');
 
 new WebpackDevServer(webpack(config), {
   contentBase: path.join(__dirname, './dist'),
