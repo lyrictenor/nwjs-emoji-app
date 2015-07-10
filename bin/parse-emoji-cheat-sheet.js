4edd0dd09e025611232bc931ce52cf8b967d9c2a
sanemat <o.gata.ken@gmail.com>
2015-07-11 00:42:04 +0900

    chore(parse): implement parsing html

--- bin/parse-emoji-cheat-sheet.js
+++ bin/parse-emoji-cheat-sheet.js
@@ -6,14 +6,27 @@ process.stdin.pipe(concat((body) => {
   const $ = cheerio.load(body.toString('utf8'));
   let emoticons = [];
   const blocks = $('ul.emojis');
-  blocks.each(function(idx, block) {
+
+  blocks.each((idx, block) => {
     let classes = $(block).attr('class').split(' ');
     let category = classes.filter((elem) => {
       return elem != 'emojis';
     }).pop();
-    $(block).each(function(i, list) {
+
+    $(block).find('li').each((i, list) => {
       let image = $(list).find('img').attr('src');
-      emoticons.push({image: image, category: category});
+      let name = $(list).find('.name');
+      let alternatives = (name.data('alternative-name') !== undefined) ?
+        String(name.data('alternative-name')).split(',').map((value) => {
+          return value.trim();
+        }) :
+        [];
+      emoticons.push({
+        name: name.text(),
+        image: image,
+        category: category,
+        alternatives: alternatives
+      });
     });
   });
   console.log(JSON.stringify(emoticons));
