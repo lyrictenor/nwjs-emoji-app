a08794997cd4f711ab72e051e19854dccf93f647
sanemat <o.gata.ken@gmail.com>
2015-07-12 01:46:45 +0900

    chore(component): build template

--- lib/components/MainSection.jsx
+++ lib/components/MainSection.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import emojiCheatSheetData from '../../emoji-cheat-sheet-data.json';
 import path from 'path';
+import IconBox from './IconBox.jsx';
 
 export default class MainSection extends Component {
   static propTypes = {
@@ -20,14 +21,23 @@ export default class MainSection extends Component {
       emoji.image = filePath;
       return emoji;
     });
+    const peopleEmojiData = emojiData.filter((emoji) => {
+      return emoji.category === 'people';
+    });
 
     return (
       <section className='main'>
         <ul className='emoji-used-list'>
           {emojis.map(emoji =>
-            <li key={emoji.text} {...actions}>{emoji.text}</li>
+              <li key={emoji.text} {...actions}>{emoji.text}</li>
           )}
         </ul>
+        <IconBox
+          key='people'
+          category='people'
+          icons={peopleEmojiData}
+          {...actions}
+          />
       </section>
     );
   }
