7da1aabe797f18f0104f253a05857ae22b9de47d
sanemat <o.gata.ken@gmail.com>
2015-07-18 11:09:47 +0900

    chore(redux): jsx class extend React.Component

--- lib/containers/App.jsx
+++ lib/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Root from './Root.jsx';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -6,7 +6,7 @@ import * as reducers from '../reducers';
 
 const store = createStore(reducers);
 
-export default class App {
+export default class App extends Component {
   render () {
     return (
       <Provider store={store}>
