a5d223254092444187dd9c656f8b90e754c4dfc4
sanemat <o.gata.ken@gmail.com>
2015-07-19 01:09:31 +0900

    feat(utils): update open externals

--- lib/components/Footer.jsx
+++ lib/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import onOpenExternals from '../utils/openExternals';
 
 export default class Footer extends Component {
   constructor (props, context) {
@@ -15,11 +16,13 @@ export default class Footer extends Component {
           <li>
             ☃ Emoji App source code is available on <a
               href="https://github.com/lyrictenor/nwjs-emoji-app"
+              onClick={onOpenExternals.bind(this)}
               >GitHub</a>.
           </li>
           <li>
             ☺ These emoji data are based on <a
               href="http://www.emoji-cheat-sheet.com/"
+              onClick={onOpenExternals.bind(this)}
               >Emoji-cheat-sheet.com</a>.
           </li>
         </ul>
