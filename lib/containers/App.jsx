10041bf6f231164273deb4726c554f5965e6d9fa
sanemat <o.gata.ken@gmail.com>
2015-07-18 11:16:29 +0900

    chore(redux): use combineReducers for createStore

--- lib/containers/App.jsx
+++ lib/containers/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Root from './Root.jsx';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import * as reducers from '../reducers';
 
-const store = createStore(reducers);
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 
 export default class App extends Component {
   render () {
